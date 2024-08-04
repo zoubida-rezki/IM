63e2a6fd047259a54b1f249b6542db0841c962ad
zoubida-rezki <zoubidaziroki@gmail.com>
2024-08-04 01:41:30 -0500

    "message" message: hello

--- my-app/firebase.js
+++ my-app/firebase.js
@@ -16,6 +16,17 @@ const firebaseConfig = {
   appId: "1:942337154894:web:9cad7f714ebd7a8ef7c81f",
   measurementId: "G-Z2WFVH9N5W"
 };
+// require('dotenv').config();
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
